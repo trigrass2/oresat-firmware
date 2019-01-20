0bea4bf4d1dba7d2dfdaef14d55d7e7ef946698a
Chad Coates <chroco@gmail.com>
2019-01-20 11:04:22 -0800

    Add magnetorquer code

--- src/f4/app_oresat_acs-2.0/main.c
+++ src/f4/app_oresat_acs-2.0/main.c
@@ -57,8 +57,21 @@ static void app_init(void)
 	pinConfig();
 
   /// Initialize CAN
-  canRPDOObjectInit(CAN_PDO_1, CAN_ID_DEFAULT, CAN_BUF_SIZE, acs.can_buf.cmd);
-	canTPDOObjectInit(CAN_PDO_1, CAN_ID_DEFAULT, 0, 0, CAN_BUF_SIZE, acs.can_buf.status);
+  canRPDOObjectInit(
+    CAN_PDO_1, 
+    CAN_ID_DEFAULT, 
+    CAN_BUF_SIZE, 
+    acs.can_buf.cmd
+  );
+	
+  canTPDOObjectInit(
+    CAN_PDO_1, 
+    CAN_ID_DEFAULT, 
+    0, 
+    0, 
+    CAN_BUF_SIZE, 
+    acs.can_buf.status
+  );
 
   acs_init(&acs);
 
